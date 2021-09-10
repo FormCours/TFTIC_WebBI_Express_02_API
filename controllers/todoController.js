de43ac478bc12284ac3a112c7594292f62e384cb
Aurélien <aurelien.strimelle@bstorm.be>
2021-09-10 13:43:25 +0200

    Add todo controller

---
+++ controllers/todoController.js
@@ -0,0 +1,24 @@
+const todoController = {
+
+    getAll: (req, res) => {
+        res.sendStatus(501);
+    },
+
+    getOne: (req, res) => {
+        res.sendStatus(501);
+    },
+
+    insert: (req, res) => {
+        res.sendStatus(501);
+    },
+
+    update: (req, res) => {
+        res.sendStatus(501);
+    },
+
+    delete: (req, res) => {
+        res.sendStatus(501);
+    }
+}
+
+export default todoController;
